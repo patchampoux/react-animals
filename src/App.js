548bc25a5527c53f614444fd0b2955b27cd2766d
patchampoux <champoux.patrick@gmail.com>
2023-07-05 22:07:12 -0400

    Finalizing Styling.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
 	}
 
 	const renderedAnimals = animals.map((animal, index) => {
-		return <AnimalShow type={animal} key={index} />
+		return <AnimalShow key={index} type={animal} />
 	});
 
 	return (
